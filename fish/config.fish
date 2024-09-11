a2c89b934a1914d072da33a18cd796c0ab1b48fa
AlejandroHerr <ahernandezc@protonmail.com>
2024-09-11 17:03:27 +0200

    setup

--- fish/config.fish
+++ fish/config.fish
@@ -1,18 +1,12 @@
-if status is-interactive
-  # Commands to run in interactive sessions can go here
-end
-set -x LC_ALL en_US.UTF-8
-set -x LC_CTYPE en_US.UTF-8
-set -x LANG en_US.UTF-8
-load_nvm > /dev/stderr
+#set -x GOPATH $HOME/go
+#set -x GOROOT /usr/local/opt/go/libexec
+set PATH $GOROOT/bin $PATH
+#set PATH $GOPATH/bin $GOROOT/bin $PATH
+#set PATH $HOME/.cargo/env $PATH
+status --is-interactive; and rbenv init - fish | source
 starship init fish | source
 
 
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
-
-# go
-set --export GOPATH "$HOME/go"
-set --export PATH $GOPATH/bin $PATH
-
