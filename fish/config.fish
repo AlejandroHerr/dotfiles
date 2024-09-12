17d7673426bcc6b0dca67f0033323c34fd8c94b8
Alejandro Hernández Cobo <alejandro@voskhod.local>
2024-09-12 17:57:40 +0200

    wip

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,23 @@
-#set -x GOPATH $HOME/go
-#set -x GOROOT /usr/local/opt/go/libexec
-set PATH $GOROOT/bin $PATH
-#set PATH $GOPATH/bin $GOROOT/bin $PATH
-#set PATH $HOME/.cargo/env $PATH
-status --is-interactive; and rbenv init - fish | source
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# homebre
+set -gx HOMEBREW_PREFIX "/opt/homebrew";
+set -gx HOMEBREW_CELLAR "/opt/homebrew/Cellar";
+set -gx HOMEBREW_REPOSITORY "/opt/homebrew";
+fish_add_path -gP "/opt/homebrew/bin" "/opt/homebrew/sbin";
+if test -n "$MANPATH[1]"; set -gx MANPATH '' $MANPATH; end;
+if not contains "/opt/homebrew/share/info" $INFOPATH; set -gx INFOPATH "/opt/homebrew/share/info" $INFOPATH; end;
+
+# nvm
+load_nvm > /dev/stderr
+
+# go
+set --export GOPATH "$HOME/go"
+fish_add_path $GOPATH
+
+# starship
+set -gx STARSHIP_CONFIG "/Users/alejandro/.config/starship.toml"
 starship init fish | source
 
-
-# bun
-set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH $BUN_INSTALL/bin $PATH
